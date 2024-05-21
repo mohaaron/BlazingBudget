230b9f6664c1030a423ff00b6eb3efb82e67c628
Aaron Prohaska <mohaaron@gmail.com>
2024-05-20 17:39:01 -0700

    Add CreditCard class

---
+++ src/Domain/BlazingBudget.Domain/Enums/CreditCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBudget.Domain.Enums;
+public abstract class CreditCard : Enumeration<CreditCard>
+{
+    public static readonly CreditCard Discover = new DiscoverCard();
+    public static readonly CreditCard Mastercard = new MastercardCard();
+    public static readonly CreditCard Visa = new VisaCard();
+
+    private CreditCard(int value, string name)
+        : base(value, name)
+    {
+        //
+    }
+
+    public abstract double Discount { get; }
+
+    private sealed class DiscoverCard : CreditCard
+    {
+        public DiscoverCard() : base(1, "Discover Card") { }
+
+        public override double Discount => 0.10;
+    }
+
+    private sealed class MastercardCard : CreditCard
+    {
+        public MastercardCard() : base(1, "Mastercard") { }
+
+        public override double Discount => 0.10;
+    }
+
+    private sealed class VisaCard : CreditCard
+    {
+        public VisaCard() : base(1, "Visa") { }
+
+        public override double Discount => 0.10;
+    }
+}
---
+++ src/Domain/BlazingBudget.Domain/Enums/Enumeration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBudget.Domain.Enums;
+public abstract class Enumeration<TEnum> : IEquatable<Enumeration<TEnum>>
+    where TEnum : Enumeration<TEnum>
+{
+    private static readonly Dictionary<int, TEnum> Enumerations = CreateEnumerations();
+
+    protected Enumeration(int value, string name)
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public int Value { get; protected init; }
+    public string Name { get; protected init; } = string.Empty;
+
+    public static TEnum? FromValue(int value)
+    {
+        return Enumerations.TryGetValue(
+            value,
+            out TEnum? enumeration) ?
+            enumeration :
+            default;
+    }
+
+    public static TEnum? FromName(string name)
+    {
+        return Enumerations
+            .Values
+            .SingleOrDefault(e => e.Name == name);
+    }
+
+    public static IEnumerable<T> GetAll<T>() where T : Enumeration<TEnum>
+        => typeof(T).GetFields(BindingFlags.Public |
+                            BindingFlags.Static |
+                            BindingFlags.DeclaredOnly)
+                 .Select(f => f.GetValue(null))
+                 .Cast<T>();
+
+    public bool Equals(Enumeration<TEnum>? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return GetType().Equals(other.GetType()) &&
+               Value == other.Value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Enumeration<TEnum> other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    private static Dictionary<int, TEnum> CreateEnumerations()
+    {
+        var enumerationType = typeof(TEnum);
+        var fieldsForType = enumerationType
+            .GetFields(
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.FlattenHierarchy)
+            .Where(fieldInfo =>
+                enumerationType.IsAssignableFrom(fieldInfo.FieldType))
+            .Select(fieldInfo =>
+                (TEnum)fieldInfo.GetValue(default)!);
+
+        return fieldsForType.ToDictionary(x => x.Value);
+    }
+}
