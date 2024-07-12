54fbb3a7c700cf5eb48b52603cf61ec295189253
Aaron Prohaska <mohaaron@gmail.com>
2024-07-12 12:03:50 -0700

    Add get budget specification

---
+++ src/Application/BlazingBudget.Application/Budgets/Specifications/GetMonthlyBudgetSpecification.cs
@@ -0,0 +1,22 @@
+﻿using BlazingBudget.Domain.Aggregates.Budgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBudget.Application.Budgets.Specifications;
+/// <summary>
+/// https://medium.com/@michaelmaurice410/implementing-the-specification-pattern-in-c-with-entity-framework-core-bac5d8522480
+/// </summary>
+public class GetMonthlyBudgetSpecification : SpecificationBase<Budget>
+{
+    public GetMonthlyBudgetSpecification(DateOnly month)
+        : base(b => b.Month == month)
+    {
+        AddInclude(b => b.Incomes);
+        AddInclude(b => b.Expenses);
+    }
+
+    
+}
---
+++ src/Application/BlazingBudget.Application/Budgets/Specifications/SpecificationBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBudget.Application.Budgets.Specifications;
+public class SpecificationBase<TEntity> where TEntity : class
+{
+    public Expression<Func<TEntity, bool>> Criteria { get; }
+    public List<Expression<Func<TEntity, object>>> Includes { get; } = new List<Expression<Func<TEntity, object>>>();
+    public Expression<Func<TEntity, object>> OrderBy { get; private set; } = default!;
+
+    public SpecificationBase(Expression<Func<TEntity, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    protected void AddInclude(Expression<Func<TEntity, object>> include)
+    {
+        Includes.Add(include);
+    }
+
+    protected void AddOrderBy(Expression<Func<TEntity, object>> orderBy)
+    {
+        OrderBy = orderBy;
+    }
+}
